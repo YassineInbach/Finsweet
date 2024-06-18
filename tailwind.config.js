71748eb0c4184c258e8c37d1bcaa4cac81b7642b
YassineInbach <yassine.inbach@gmail.com>
2024-06-18 13:25:49 +0100

    tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,pug}"],
+  theme: {
+    colors : {
+      white : '#FFFFFF',
+      'royal-blue' : '#2405F2',
+      'dark-blue' : '#282938',
+      grey : '#F4F6FC',
+      yellow : '#FCD980',
+      'tint-blue' : '#1C1E53',
+      accent : '#EEF4FA'
+    },
+    fontSize : {
+      xs : '16px',
+      sm : '18px',
+      base : '24px',
+      lg : '32px',
+      xl : '36px',
+      '2xl' : '38px',
+      '3xl' : '48px',
+      '4xl' : '54px'
+    },
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    // Activer le layer 'base'
+    preflight: false,
+  },
+}

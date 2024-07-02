6de0fbe05bf0f995bbf5669134d469b42a7924e7
YassineInbach <yassine.inbach@gmail.com>
2024-07-02 10:00:57 +0100

    slick.js

---
+++ src/assets/js/slick.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  
+  // Initialize single-item slick slider
+  function initializeSingleItemSlider() {
+    $('.single-item').slick({
+      speed: 2000,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: $('.slick-prev'),
+      nextArrow: $('.slick-next'),
+    });
+  }
+
+  // Initialize multiple-items slick slider
+  function initializeMultipleItemsSlider() {
+    $('.multiple-items').slick({
+      speed: 2000,
+      infinite: true,
+      slidesToShow:3,
+      slidesToScroll: 1,
+      prevArrow: $('.slick-prev'),
+      nextArrow: $('.slick-next'),
+      responsive : [
+        {
+          breakpoint : 923,
+          settings : {
+            slidesToShow:2
+            
+          }
+        },
+        {
+          breakpoint : 638,
+          settings : {
+            slidesToShow : 1 
+          }
+        }
+      ]
+    });
+  }
+
+  // Call initialization functions
+  initializeSingleItemSlider();
+  initializeMultipleItemsSlider();
+
+});
+
